6308201880095aad1456a812e5530fc6d5d85e9f
jun-c0de <sh48520414@gmail.com>
2025-08-06 12:01:30 +0900

    UPDATE

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Section from './conponents/Section'
+
+function App() {
+
+  return (
+    <div>
+      <Section/>
+      <h1>Hello React!</h1>
+    </div>
+  )
+}
+
+export default App
